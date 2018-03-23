d4f613b61e8556ce853798bf8ce41cccea30a83e
sanchescom <326551632>
2018-03-23 17:27:53 +0300

    currency table migration and fetch file

---
+++ app/Console/Commands/FetchCurrencies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FetchCurrencies extends Command
+{
+    protected $signature = 'fetch:currency';
+    protected $description = 'Fetching currencies for user accounts';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+}
---
+++ database/migrations/2018_03_23_110600_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('rate');
+            $table->char('currency', 3);
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
